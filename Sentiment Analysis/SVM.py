f7dc5cc1c5f97aab0f2cc04126948c4d8c1f0384
vanrhyga <vanrhyga@icloud.com>
2018-08-13 10:48:41 +0800

    tmp

--- Sentiment Analysis/SVM.py
+++ Sentiment Analysis/SVM.py
@@ -26,24 +26,22 @@ def reviewToWordlist(review):
 
 
 #Import the data.
-data1=pd.read_csv('trainSet1.csv',header=0,delimiter='\t',quoting=3)
-data2=pd.read_csv('trainSet2.csv',header=0,delimiter='\n',quoting=3)
+data1=pd.read_excel('trainSet1.xlsx',header=1,skiprows=0)
+data2=pd.read_excel('trainSet2.xlsx',header=1,skiprows=0)
 
-data=data1.append(data2)
+trainSet=data1.append(data2)
 
-print(data)
-
-trainSet,devSet= train_test_split(data, test_size=0.1, random_state=0)
+testSet=pd.read_csv('test.csv',header=0,delimiter='\t',quoting=3)
 
 trainSet=trainSet.reset_index(drop=True)
-devSet=devSet.reset_index(drop=True)
+testSet=testSet.reset_index(drop=True)
 
 trainData=[]
 for i in range(0,len(trainSet['review'])):
     trainData.append(" ".join(reviewToWordlist(trainSet['review'][i])))
-devData=[]
-for i in range(0,len(devSet['review'])):
-    devData.append(" ".join(reviewToWordlist(devSet['review'][i])))
+testData=[]
+for i in range(0,len(testSet['review'])):
+    testData.append(" ".join(reviewToWordlist(testSet['review'][i])))
 
 tfv=TFIV(min_df=3,max_features=None,
          strip_accents='unicode',analyzer='word',token_pattern=r'\w{1,}',
@@ -51,14 +49,14 @@ tfv=TFIV(min_df=3,max_features=None,
          stop_words='english')
 
 #Combine both to fit the TFIDF vectorization.
-allData=trainData+devData
+allData=trainData+testData
 lenTrain=len(trainData)
 tfv.fit(allData)
 allData=tfv.transform(allData)
 
 #Separate back into training and dev sets.
 train=allData[:lenTrain]
-dev=allData[lenTrain:]
+test=allData[lenTrain:]
 
 #Regularization parameter
 sgdParams={'alpha':[0.00006,0.00007,0.00008,0.0001,0.0005]}
@@ -69,7 +67,7 @@ modelSGD=GridSearchCV(SGD(random_state=0,shuffle=True,loss='modified_huber'),
 
 #Fit the model.
 modelSGD.fit(train,trainSet['sentiment'])
-SGDResult=modelSGD.predict_proba(dev)[:,1]
-SGDOutput=pd.DataFrame(data={"review":devSet['review'],"sentiment":SGDResult})
-SGDOutput.to_csv("Result.csv",index=False,quoting=3)
+SGDResult=modelSGD.predict_proba(test)[:,1]
+SGDOutput=pd.DataFrame(data={"id":testSet['id'],"review":testSet['review'],"sentiment":SGDResult})
+SGDOutput.to_excel("Result.xlsx",sheet_name='Result',index=False)
 print(modelSGD.best_score_)
